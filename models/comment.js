cda63ebd6c19c7f5a4759014b2d09b7820eca90b
Ruturaj Jadhav <ruturajjadhav2003@yahoo.in>
2017-10-27 14:25:51 +0000

    add moment

--- models/comment.js
+++ models/comment.js
@@ -1,8 +1,9 @@
-var mongoose = require("mongoose");
+    var mongoose = require("mongoose");
 
 var commentSchema = new mongoose.Schema(
     {
         text: String,
+        createdAt: { type: Date, default: Date.now },
         author: {
             id: {
                 type: mongoose.Schema.Types.ObjectId,
