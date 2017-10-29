04af1ee16ce94eca1ec326b340da7f9465ac384b
Ruturaj Jadhav <ruturajjadhav2003@yahoo.in>
2017-10-29 17:53:14 +0000

    add user profile

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,6 @@
 var Hotel   = require("../models/hotel"),
-    Comment = require("../models/comment");
+    Comment = require("../models/comment"),
+    User    = require("../models/user");
 
 var middlewareObj = {};
 
@@ -32,6 +33,27 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
                 res.redirect("back");
             } else{
                 if(foundComment.author.id.equals(req.user.id) || req.user.isAdmin){
+                    next();
+                } else{
+                    req.flash("error", "You don't have permission to do that");
+                    res.redirect("back");
+                }
+            }
+        })
+    } else{
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkUserOwnership = function(req, res, next) {
+    if(req.isAuthenticated()){
+        User.findById(req.params.id, function(err, foundUser) {
+            if(err || !foundUser){
+                req.flash("error", "User not found");
+                res.redirect("back");
+            } else{
+                if(foundUser._id.equals(req.user.id)){
                     next();
                 } else{
                     req.flash("error", "You don't have permission to do that");
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express     = require("express"),
     router      = express.Router(),
     passport    = require("passport"),
-    User        = require("../models/user");
+    User        = require("../models/user"),
+    Hotel       = require("../models/hotel"),   
+    middleware  = require("../middleware");
 
 router.get("/", function (req, res) {
     res.render("landing");
@@ -14,7 +16,14 @@ router.get("/register", function(req, res) {
 });
 
 router.post("/register", function(req, res) {
-    var newUser = new User({username: req.body.username});
+    var newUser = new User(
+        {
+            username: req.body.username,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            avatar: req.body.avatar
+        });
     if(req.body.adminCode === "secretcode") {
         newUser.isAdmin = true;
     }
@@ -49,6 +58,66 @@ router.get("/logout", function(req, res) {
     req.logout();
     req.flash("success", "See you later!");
     res.redirect("/hotels");
+});
+
+//user profile
+//show
+router.get("/users/:id", function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+        if(err){
+            req.flash("error", "User not found");
+            console.log(err);
+            res.redirect("back");
+        } else {
+            Hotel.find().where("author.id").equals(user._id).exec(function (err, hotels) {
+                if(err){
+                    req.flash("error", "Something went wrong");
+                    console.log(err);
+                    res.redirect("back");
+                } else{
+                    res.render("users/show", {user: user , hotels: hotels});
+                }
+            });
+        }
+    });
+});
+
+//edit
+router.get("/users/:id/edit", middleware.checkUserOwnership, function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+        if(err){
+            req.flash("error", "User not found");
+            console.log(err);
+            res.redirect("back");
+        } else {
+            res.render("users/edit", {user: user});
+        }
+    });
+});
+
+//update
+router.post("/users/:id", middleware.checkUserOwnership, function(req, res) {
+    var data = 
+        {
+            username: req.body.username,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            avatar: req.body.avatar
+        };
+    if(req.body.adminCode === "secretcode") {
+        data.isAdmin = true;
+    }
+    User.findByIdAndUpdate(req.params.id, data, function(err, updatedUser){
+        if(err) {
+            req.flash("error", "Something went wong");
+            console.log(err);
+            res.redirect("back");
+        } else {
+                req.flash("success", "Profile updated");
+                res.redirect("/users/" + req.params.id);    
+        }
+    });
 });
 
 module.exports = router;
